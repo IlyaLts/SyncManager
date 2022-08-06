9c3502c7bca61913feb5c04268f759fa40df7e56
Ilya Lyakhovets <ilyalyakhovets@gmail.com>
2022-08-06 09:30:20 +0700

    Shrink File structure from 48 to 40 bytes, which reduces memory usage a little bit

--- MainWindow.h
+++ MainWindow.h
@@ -44,20 +44,22 @@ class QItemSelection;
 
 struct File
 {
-    enum Type
-    {
-        none,
-        file,
-        dir
-    } type = none;
+    enum Type : int;
 
     File(){}
-    File(QString path, Type type, QDateTime time, bool updated = false) : type(type), path(path), date(time), updated(updated), exists(true){}
+    File(QString path, Type type, QDateTime time, bool updated = false) : path(path), date(time), updated(updated), exists(true), type(type){}
 
     QString path;
     QDateTime date;
     bool updated = false;
     bool exists = false;
+
+    enum Type : int
+    {
+        none,
+        file,
+        dir
+    } type = none;
 };
 
 struct Folder
