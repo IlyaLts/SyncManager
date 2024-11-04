2d5af8e49a809f066d679a6e6eb07403e31836a9
Ilya Lyakhovets <ilyalyakhovets@gmail.com>
2024-11-04 11:28:22 +0700

    Update version

--- MainWindow.h
+++ MainWindow.h
@@ -26,7 +26,7 @@
 #include <QTimer>
 #include <QMovie>
 
-#define SYNCMANAGER_VERSION     "1.9.2"
+#define SYNCMANAGER_VERSION     "1.9.3"
 #define SETTINGS_FILENAME       "Settings.ini"
 #define PROFILES_FILENAME       "Profiles.ini"
 #define UPDATE_DELAY            40
